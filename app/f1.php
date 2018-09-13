99f2adb403610dc05e8a184f305b2441aa7d11dc
okazakikirito <okazakikirito@gmail.com>
2018-09-13 21:11:23 +0530

    new

---
+++ app/f1.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class f1 extends Model
+{
+    //
+}
---
+++ tests/Feature/Auth/LoginTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+
+
+class LoginTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    // public function testExample()
+    // {
+    //     $this->assertTrue(true);
+    // }
+
+
+    public function test_user_can_view_a_login_form()
+    {
+        $response = $this->get('/login');
+        $response->assertSuccessful();
+        $response->assertViewIs('auth.login');
+    }
+
+ 
+
+    // public function test_user_cannot_view_a_login_form_when_authenticated()
+    // {
+    //     $user = factory(User::class)->make();
+    //     $response = $this->actingAs($user)->get('/login');
+    //     $response->assertRedirect('/home');
+    // }
+}
